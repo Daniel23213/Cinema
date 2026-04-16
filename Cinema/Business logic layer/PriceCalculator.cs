243ff4a215c8b90831d8ef191d27e096ffdc4882
Andrei-ierdnA <an.plchkv@gmail.com>
2026-04-16 12:32:40 +0200

    Made Seat class allign with ERD and added PriceCalculator class to seperated logic from seat

---
+++ Cinema/Business logic layer/PriceCalculator.cs
@@ -0,0 +1,14 @@
+﻿public static class PriceCalculator
+{
+    // you can overload this method to allow for additional requirements when calculating the price
+    public static int CalculatePrice(string seatType)
+    {
+        return seatType switch
+        {
+            "normal" => 12,
+            "luxe" => 12,
+            "luxe plus" => 12,
+            _ => throw new ArgumentException($"'{seatType}' is not a valid seat type.")
+        };
+    }
+}
