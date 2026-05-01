c28499f6c7878fc1ddb421308be9ff4e9c5a4521
Bialywilku412 <maciekplewka0702@gmail.com>
2026-05-01 10:25:10 +0200

    deleting updating movies

--- Cinema/Data Access layer/MovieAcess.cs
+++ Cinema/Data Access layer/MovieAcess.cs
@@ -1,58 +1,85 @@
 using Microsoft.Data.Sqlite;
 
-public class MovieAcess
+public class MovieAcces : IMovieAcces
 {
-    private const string ConnectionString = "Data Source=../../../Data Source/Cinema.db";
-
-    public void AddMovie(string title, string author, string genre, TimeSpan duration, DateTime premier)
-    {
-        using var connection = new SqliteConnection(ConnectionString);
-        connection.Open();
-
-        using var command = connection.CreateCommand();
-        command.CommandText = @"
-            INSERT INTO movies (Title, Duration, Author, Genre, Premier)
-            VALUES (@title, @duration, @author, @genre, @premier);
-        ";
-
-        command.Parameters.AddWithValue("@title", title);
-        command.Parameters.AddWithValue("@duration", duration.ToString());
-        command.Parameters.AddWithValue("@author", author);
-        command.Parameters.AddWithValue("@genre", genre);
-        command.Parameters.AddWithValue("@premier", premier);
-
-        command.ExecuteNonQuery();
-    }
+    private const string ConnectionString = "../../../Data Source/Cinema.db";
 
     public List<MovieModel> GetAiringMovies()
     {
-        List<MovieModel> movies = new List<MovieModel>();
+        var movies = new List<MovieModel>();
 
         using var connection = new SqliteConnection(ConnectionString);
         connection.Open();
 
-        using var command = connection.CreateCommand();
-        command.CommandText = @"
-            SELECT Id, Title, Author, Genre, Duration, Premier
-            FROM movies
-        ";
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT Id, Title, Author, Genre, Duration, Premier FROM movies";
 
         using var reader = command.ExecuteReader();
 
         while (reader.Read())
         {
-            MovieModel movie = new MovieModel(
+            movies.Add(new MovieModel(
                 reader.GetInt32(0),
                 reader.GetString(1),
                 reader.GetString(2),
                 reader.GetString(3),
                 TimeSpan.Parse(reader.GetString(4)),
                 DateTime.Parse(reader.GetString(5))
-            );
-
-            movies.Add(movie);
+            ));
         }
 
         return movies;
+    }
+
+    public void AddMovie(string title, string author, string genre, TimeSpan duration, DateTime premier)
+    {
+        using var connection = new SqliteConnection(ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            INSERT INTO movies (Title, Duration, Author, Genre, Premier)
+            VALUES (@title, @duration, @author, @genre, @premier)";
+
+        command.Parameters.AddWithValue("@title", title);
+        command.Parameters.AddWithValue("@duration", duration.ToString());
+        command.Parameters.AddWithValue("@author", author);
+        command.Parameters.AddWithValue("@genre", genre);
+        command.Parameters.AddWithValue("@premier", premier.ToString("yyyy-MM-dd HH:mm:ss"));
+
+        command.ExecuteNonQuery();
+    }
+
+    public void UpdateMovie(int id, string title, string author, string genre, TimeSpan duration, DateTime premier)
+    {
+        using var connection = new SqliteConnection(ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            UPDATE movies
+            SET Title=@title, Author=@author, Genre=@genre, Duration=@duration, Premier=@premier
+            WHERE Id=@id";
+
+        command.Parameters.AddWithValue("@id", id);
+        command.Parameters.AddWithValue("@title", title);
+        command.Parameters.AddWithValue("@author", author);
+        command.Parameters.AddWithValue("@genre", genre);
+        command.Parameters.AddWithValue("@duration", duration.ToString());
+        command.Parameters.AddWithValue("@premier", premier.ToString("yyyy-MM-dd HH:mm:ss"));
+
+        command.ExecuteNonQuery();
+    }
+
+    public void DeleteMovie(int id)
+    {
+        using var connection = new SqliteConnection(ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "DELETE FROM movies WHERE Id=@id";
+        command.Parameters.AddWithValue("@id", id);
+
+        command.ExecuteNonQuery();
     }
 }
--- Cinema/Presentationn layer/Menu.cs
+++ Cinema/Presentationn layer/Menu.cs
@@ -1,92 +1,25 @@
 ﻿//using Cinema.Presentationn_layer;
 
-public static class Menu 
+public static class Menu
 {
-    private static void AddMovieMenu()
-    {
-        Console.Write("Enter title: ");
-        string title = Console.ReadLine();
-
-        Console.Write("Enter author: ");
-        string author = Console.ReadLine();
-
-        Console.Write("Enter genre: ");
-        string genre = Console.ReadLine();
-
-        Console.Write("Enter duration in minutes: ");
-        int minutes = int.Parse(Console.ReadLine());
-        TimeSpan duration = TimeSpan.FromMinutes(minutes);
-
-        Console.Write("Enter premiere date (yyyy-MM-dd): ");
-        DateTime premier = DateTime.Parse(Console.ReadLine());
-
-        MovieServiceLogic service = new MovieServiceLogic();
-
-        service.AddMovie(title, author, genre, duration, premier);
-
-        Console.WriteLine("✅ Movie added successfully!");
-    }
-
-    private static void ShowAiringMovies()
-    {
-        var service = new MovieServiceLogic();
-
-        bool inScreen = true;
-
-        while (inScreen)
-        {
-            Console.Clear();
-            Console.WriteLine("\n🎬 Airing Movies:\n");
-
-            var movies = service.GetAiringMovies();
-
-            if (movies.Count == 0)
-            {
-                Console.WriteLine("No airing movies right now.");
-                return;
-            }
-            else
-            {
-                foreach (var movie in movies)
-                {
-                    Console.WriteLine(movie);
-                }
-            }
-
-            Console.WriteLine("\n[E] Go back");
-
-            Console.Write("\nChoose: ");
-            string input = Console.ReadLine();
-
-            switch (input)
-            {
-                case "E":
-                    inScreen = false;
-                    break;
-                default:
-                    Console.WriteLine("Invalid option...");
-                    break;
-            }
-        }
-        Console.Clear();
-    }
+    private static MovieMenu movieMenu = new MovieMenu();
 
     public static void ShowMenu()
     {
         bool running = true;
-        UserModel isLogged = null ;
+        UserModel isLogged = null;
 
         while (running)
         {
             // User first Name and Last Name print put up in another if not to confuse the user
-            if(isLogged != null) 
+            if (isLogged != null)
             {
                 Console.WriteLine($"Logged in as: {isLogged.FirstName} {isLogged.LastName}");
             }
             Console.WriteLine("\n[1]: Airing movies");
             if (isLogged != null)
             {
-                
+
                 Console.WriteLine("[2]: Buy tickets");
                 Console.WriteLine("[4]: Cancel tickets");
                 Console.WriteLine("[3]: Booked tickets");
@@ -112,7 +45,7 @@ public static class Menu
             }
 
             //use this when UserRole will be implemented
-            
+
 
 
             Console.Write("Choose an option: ");
@@ -121,9 +54,7 @@ public static class Menu
             switch (input)
             {
                 case "1":
-                    //Add movie will go to adming
-                    AddMovieMenu();
-                    ShowAiringMovies();
+                    movieMenu.GetAiringMovies();
                     break;
 
                 case "2":
@@ -155,7 +86,7 @@ public static class Menu
                     if (manageInput == "D" || manageInput == "d")
                     {
                         // Implement delete account
-                        
+
                         userAccess.Delete(isLogged);
                         Console.WriteLine("Your account has been deleted!");
                         isLogged = null;
@@ -174,19 +105,30 @@ public static class Menu
                     //Register sysyem
 
                     isLogged = RegisterMenu.ShowRegisterMenu();
-                    
-                   
+
+
                     break;
                 case "L" or "l":
-                   //login sysyem
-            
-                        isLogged = LoginMenu.Show();
-                    
+                    //login sysyem
+
+                    isLogged = LoginMenu.Show();
+
                     break;
                 case "U" or "u":
 
-                     ManageUsers.Show();
+                    ManageUsers.Show();
 
+                    break;
+                case "M" or "m":
+                    if (isLogged != null &&
+                        (isLogged.Role == "Admin" || isLogged.Role == "SuperManager"))
+                    {
+                        movieMenu.ShowManageMoviesMenu();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Access denied.");
+                    }
                     break;
 
                 //use this when UserRole will be implemented
