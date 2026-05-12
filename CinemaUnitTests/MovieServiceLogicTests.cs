7289a2f49c491861a2bbaf63954ea2ec2dca715b
Andrei-ierdnA <an.plchkv@gmail.com>
2026-05-12 10:03:19 +0200

    Added tests files for each logic file

---
+++ CinemaUnitTests/MovieServiceLogicTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+[TestClass]
+public class MovieServiceLogicTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ CinemaUnitTests/PriceCalculatorLogicTests.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+[TestClass]
+public class PriceCalculatorLogicTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
