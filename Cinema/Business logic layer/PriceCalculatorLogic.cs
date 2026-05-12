751dfc3ef5f7aae717cc377b84050f32972338ef
Andrei-ierdnA <an.plchkv@gmail.com>
2026-05-12 10:45:49 +0200

    Added discount option for senior and child

--- Cinema/Business logic layer/PriceCalculatorLogic.cs
+++ Cinema/Business logic layer/PriceCalculatorLogic.cs
@@ -5,8 +5,17 @@
     luxePlus = 16,
 }
 
+public enum Ages
+{
+    senior = 65,
+    child = 17,
+}
+
 public static class PriceCalculatorLogic
 {
+    private const decimal seniorDiscount = 0.85M;
+    private const decimal childDiscount = 0.85M;
+
     // you can overload this method to allow for additional requirements when calculating the price
     public static decimal CalculatePrice(string seatType)
     {
@@ -17,5 +26,21 @@ public static class PriceCalculatorLogic
             "luxe plus" => (int)Prices.luxePlus,
             _ => throw new ArgumentException($"'{seatType}' is not a valid seat type.")
         };
+    }
+
+    public static decimal CalculatePrice(string seatType, int age)
+    {
+        decimal basePrice = CalculatePrice(seatType);
+
+        if (age >= (int)Ages.senior)
+        {
+            return basePrice * seniorDiscount;
+        }
+        if (age <= (int)Ages.child)
+        {
+            return basePrice * childDiscount;
+        }
+
+        return basePrice;
     }
 }
