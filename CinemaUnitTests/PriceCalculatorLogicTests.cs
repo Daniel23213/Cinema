ab18fa9dab5ba8ebc80ffa0655fc7040573399fd
Andrei-ierdnA <an.plchkv@gmail.com>
2026-06-02 14:16:37 +0200

    added new test sbut not removed old ones

--- CinemaUnitTests/PriceCalculatorLogicTests.cs
+++ CinemaUnitTests/PriceCalculatorLogicTests.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
 
-[TestClass]
-public class PriceCalculatorLogicTests
-{
-    [TestMethod]
-    public void TestMethod1()
-    {
-    }
-}
+//[TestClass]
+//public class PriceCalculatorLogicTests
+//{
+//    [TestMethod]
+//    public void TestMethod1()
+//    {
+//        Console.WriteLine("test");
+//    }
+//}
