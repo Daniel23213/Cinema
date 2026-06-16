b15ac95d4299eb516e458664cc5b5d13bcc0f883
Andrei-ierdnA <an.plchkv@gmail.com>
2026-06-16 10:11:34 +0200

    Made 2 validation method, one for string, other for ints, applied themi RegisterMenu

---
+++ Cinema/Presentationn layer/UserInputValidation.cs
@@ -0,0 +1,30 @@
+﻿public static class UserInputValidation
+{
+    public static string NullOrEmptyValidationLoop(string message)
+    {
+        string input = null;
+        do
+        {
+            Console.WriteLine(message);
+
+            input = Console.ReadLine();
+        }
+        while (string.IsNullOrWhiteSpace(input));
+
+        return input;
+    }
+
+    public static int IntInputValidation(string message)
+    {
+        string inputString = null;
+        int inputInt = default(int);
+
+        do
+        {
+            inputString = UserInputValidation.NullOrEmptyValidationLoop(message);
+        }
+        while (!int.TryParse(inputString, out inputInt));
+
+        return inputInt;
+    }
+}
