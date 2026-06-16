05a982c6ed10b7780575c30b76b375997a6f9d4e
shuyawei1227-dotcom <shuyawei1227@gmail.com>
2026-06-16 10:00:23 +0200

    Changed file paths for all other files

--- Cinema/Data Access layer/MovieAcess.cs
+++ Cinema/Data Access layer/MovieAcess.cs
@@ -3,7 +3,7 @@ using Microsoft.Data.Sqlite;
 public class MovieAcces : IMovieAcces
 {
     private const string ConnectionString =
-        "Data Source=../../../Data Source/Cinema.db";
+        "Data Source=./Data Source/Cinema.db";
 
     public List<MovieModel> GetAiringMovies()
     {
@@ -149,7 +149,7 @@ public class MovieAcces : IMovieAcces
             double extraPrice = reader.GetDouble(7);
 
             string culinaryText = isCulinary
-                ? $" | Culinary Cinema (+€{extraPrice})"
+                ? $" | Culinary Cinema (+ï¿½{extraPrice})"
                 : "";
 
             int age = reader.GetInt32(2);
@@ -209,7 +209,7 @@ public class MovieAcces : IMovieAcces
             double extraPrice = reader.GetDouble(7);
 
             string culinaryText = isCulinary
-                ? $" | Culinary Cinema (+€{extraPrice})"
+                ? $" | Culinary Cinema (+ï¿½{extraPrice})"
                 : "";
 
             int age = reader.GetInt32(2);
