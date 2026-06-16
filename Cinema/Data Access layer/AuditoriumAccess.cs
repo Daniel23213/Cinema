19adf0a8be06de37b5472ee33008b56279b42a2b
Bialywilku412 <maciekplewka0702@gmail.com>
2026-06-16 13:28:40 +0200

    Revert "Merge pull request #57 from Daniel23213/update_initialiser"
    This reverts commit 3eaccb4f2164ef69a8be827fb069ae87d45eb4aa, reversing
    changes made to bb997581c097dc1171349a4921c002df5010efcf.

--- Cinema/Data Access layer/AuditoriumAccess.cs
+++ Cinema/Data Access layer/AuditoriumAccess.cs
@@ -2,7 +2,7 @@ using Dapper;
 using Microsoft.Data.Sqlite;
 public class AuditoriumAccess
 {
-    private const string _databaseLoc = "./Data Source/Cinema.db";
+    private const string _databaseLoc = "./Cinema.db";
     
     public AuditoriumModel GetAuditoriumByID(int id)
     {
