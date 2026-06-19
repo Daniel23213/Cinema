0daa7a5a5bb01cacaabf2804b631bf22fba07881
Andrei-ierdnA <an.plchkv@gmail.com>
2026-06-19 10:35:11 +0200

    Fixed my merging issue which removed the hashing for changing password

--- Cinema/Presentationn layer/Menu.cs
+++ Cinema/Presentationn layer/Menu.cs
@@ -288,13 +288,14 @@ public static class Menu
                     }
                     else if (manageInput.ToLower() == "c")
                     {
-                        string newpassword = UserInputValidation.NullOrEmptyValidationLoop("Enter the new password (or B to back): "); // Validation loop
+                        Console.Write("Enter the new password(Must be atleast 6 characters long): ");
+                        string newpassword = RegisterMenu.CreateMyPasswordTextBox();
 
                         if (newpassword.ToLower() == "b") { break; } // Check for back
 
                         userAccess.ChangePassword(isLogged.Id, newpassword);
 
-                        Console.WriteLine("Password changed!");
+                        Console.WriteLine("\nPassword changed!");
                         Pause(); // Consistent pause
                     }
 
